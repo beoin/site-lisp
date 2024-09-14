1071cd5787daab582ebc57f28381bffc45bc17e4
beoin <hi.beoin@gmail.com>
2024-09-14 17:30:38 +0100

    Replace setq with defvar

--- vars.el
+++ vars.el
@@ -5,8 +5,8 @@
 (add-to-list 'auto-mode-alist '("\\.tpl\\'" . web-mode))
 (add-to-list 'auto-mode-alist '("\\.heex\\'" . web-mode))
 ;;(setq web-mode-enable-current-column-highlight t)
-(setq web-mode-enable-current-element-highlight t)
-(setq web-mode-enable-auto-pairing t)
+(defvar web-mode-enable-current-element-highlight t)
+(defvar web-mode-enable-auto-pairing t)
 
 ;; Search
 (setq lazy-highlight-initial-delay 5.00)
@@ -18,14 +18,14 @@
       completion-category-overrides '((file (styles basic partial-completion))))
 
 ;; Org
-(setq org-agenda-files "~/Org/agenda-files.org")
-(setq org-directory "~/Org")
-(setq org-use-tag-inheritance nil)
-(setq org-hide-emphasis-markers t)
-(setq org-startup-folded t)
-(setq org-return-follows-link nil)
+(defvar org-agenda-files "~/Org/agenda-files.org")
+(defvar org-directory "~/Org")
+(defvar org-use-tag-inheritance nil)
+(defvar org-hide-emphasis-markers t)
+(defvar org-startup-folded t)
+(defvar org-return-follows-link nil)
 (global-auto-revert-mode t)
-(setq org-imenu-depth 3)
+(defvar org-imenu-depth 3)
 
 ;; Sessions
 (savehist-mode +1)
@@ -41,7 +41,7 @@
 (global-display-line-numbers-mode -1)
 (fset 'yes-or-no-p 'y-or-n-p)
 (setq ring-bell-function 'ignore)
-(setq modus-themes-common-palette-overrides
+(defvar modus-themes-common-palette-overrides
       '((fg-heading-1 "#A3C7D6")))
 
 ;; Files
@@ -54,14 +54,14 @@
 (setq custom-file (concat user-emacs-directory "custom.el"))
 
 ;; Company
-(setq company-idle-delay nil)
+(defvar company-idle-delay nil)
 
 ;; Programming
-(setq zig-zig-bin "~/Zig/zig-master/zig")
+;;(customize-set-variable zig-zig-bin "~/Zig/zig-master/zig")
 (electric-pair-mode)
-(setq xref-show-definitions-function #'xref-show-definitions-completing-read)
-(setq flymake-shellcheck-path "usr/bin/shellcheck")
-(setq gofmt-command "goimports")
+(defvar xref-show-definitions-function #'xref-show-definitions-completing-read)
+(defvar flymake-shellcheck-path "usr/bin/shellcheck")
+(defvar gofmt-command "goimports")
 (global-flycheck-mode +1)
 
 ;; Version Control
@@ -75,7 +75,7 @@
 (which-function-mode t)
 
 ;; Shell
-(setq eshell-aliases-file "~/.emacs.d/eshell/alias")
+;;(customize-set-variable eshell-aliases-file "~/.emacs.d/eshell/alias")
 
 ;; Snippets
 (yas-global-mode 1)
@@ -84,7 +84,7 @@
 (add-to-list 'auto-mode-alist '("\\.ejs\\'" . js-mode))
 
 ;; Common Lisp
-(setq inferior-lisp-program "/usr/bin/sbcl")
+(defvar inferior-lisp-program "/usr/bin/sbcl")
 
 (provide 'vars)
 ;;; vars.el ends here
