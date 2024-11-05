80ca9e8653c4c75a74f54f599ebe3febd522ee69
beoin <hi.beoin@gmail.com>
2024-11-05 08:31:18 +0000

    performance tweaks

--- early-init.el
+++ early-init.el
@@ -4,12 +4,17 @@
 ;;; Code:
 (setq gc-cons-threshold most-positive-fixnum)
 
+(setq load-prefer-newer t)
+(set-language-environment "UTF-8")
+(setq default-input-method nil)
+(setq inhibit-compacting-font-caches t)
+
+;; UI
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (setq inhibit-startup-screen t)
 (setq inhibit-spash-screen t)
-(customize-set-variable 'load-prefer-newer t)
 
 (provide 'early-init)
 ;;; early-init.el ends here
