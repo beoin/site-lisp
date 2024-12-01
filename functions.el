d2f2132ae88906b69e158c4a698ec7020c8d62bd
beoin <hi.beoin@gmail.com>
2024-12-01 22:36:46 +0000

    add toggle-window-split. Move from a horizontal to a vertical split and vice versa.

--- functions.el
+++ functions.el
@@ -126,5 +126,31 @@ MODE must be a symbol"
           (error (save-buffers-kill-emacs))))
   (select-frame-set-input-focus (selected-frame)))
 
+(defun toggle-window-split ()
+  "Move from a horizontal to a vertical split and vice versa."
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 (provide 'functions)
 ;;; functions.el ends here
