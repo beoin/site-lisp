c032a60743e85c68afde7c3ad5e3cf26353082cb
beoin <hi.beoin@gmail.com>
2024-12-21 17:02:40 +0000

    add function delete-this-buffer-and-file

--- functions.el
+++ functions.el
@@ -152,5 +152,18 @@ MODE must be a symbol"
           (select-window first-win)
           (if this-win-2nd (other-window 1))))))
 
+(defun delete-this-buffer-and-file ()
+  "Remove file connected to current buffer and kill buffer."
+  (interactive)
+  (let ((filename (buffer-file-name))
+        (buffer (current-buffer))
+        (name (buffer-name)))
+    (if (not (and filename (file-exists-p filename)))
+        (error "Buffer '%s' is not visiting a file!" name)
+        (when (yes-or-no-p "Are you sure you want to remove this file? ")
+          (delete-file filename)
+          (kill-buffer buffer)
+          (message "File '%s' successfully removed" filename)))))
+
 (provide 'functions)
 ;;; functions.el ends here
