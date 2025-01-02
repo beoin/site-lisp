f6f24ab00524a203743a27ea8f5f9ecab27ea6ad
beoin <hi.beoin@gmail.com>
2025-01-02 15:57:37 +0000

    add go-ts-mode hook and setup some local keybindings

--- go-conf.el
+++ go-conf.el
@@ -9,5 +9,11 @@
 ;; Vars
 (add-to-list 'auto-mode-alist '("\\.go\\'" . go-ts-mode))
 
+;; Hooks
+(add-hook 'go-ts-mode
+          (defun go-hook ()
+            (keymap-local-set "C-c f" #'gofmt)
+            (keymap-local-set "<f7>" #'goto-line)))
+
 (provide 'go-conf)
 ;;; go-conf.el ends here
