4380864a240a050914dd14a7d890f3c60e8cb567
beoin <hi.beoin@gmail.com>
2025-01-03 08:24:26 +0000

    set local for comple-command

--- go-conf.el
+++ go-conf.el
@@ -13,7 +13,8 @@
 (add-hook 'go-ts-mode-hook
           (defun go-hook ()
             (keymap-local-set "C-c f" #'gofmt)
-            (keymap-local-set "<f7>" #'goto-line)))
+            (keymap-local-set "<f7>" #'goto-line)
+            (setq-local compile-command "go run .")))
 
 (provide 'go-conf)
 ;;; go-conf.el ends here
