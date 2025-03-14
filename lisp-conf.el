4ecdb2de51d898f00e7590a95f3455f0ce0313a1
beoin <hi.beoin@gmail.com>
2025-03-14 07:52:01 +0000

    update vars, require files and add doc strings

--- lisp-conf.el
+++ lisp-conf.el
@@ -6,11 +6,19 @@
 
 ;; Vars
 (add-to-list 'auto-mode-alist '("\\.scrbl\\'" . racket-hash-lang-mode))
-(defvar inferior-lisp-program "/usr/bin/sbcl")
-(defvar delete-pair-blink-delay 0.25)
+
+;; lisp.el Lisp editing commands for Emacs
+(setq delete-pair-blink-delay 0.25)
+
+;; an inferior-lisp mode
+(require 'inf-lisp)
+(setq inferior-lisp-program "/usr/bin/sbcl")
 
 ;; Paren Matching
 (setq blink-matching-paren 'jump)
+
+;; highlight matching paren
+(require 'paren)
 (setq show-paren-when-point-inside-paren t)
 (setq show-paren-style 'parenthesis)
 
@@ -23,6 +31,8 @@
           (defun racket-hash-lang-hook ()
             (setq-local racket-xp-add-binding-faces t)))
 
+;; minor mode for editing parentheses
+(require 'paredit)
 (add-hook 'paredit-mode-hook
           (defun paredit-hook ()
             (keymap-unset paredit-mode-map "M-r" t)
@@ -60,6 +70,7 @@
             (keymap-local-set "<return>" #'read--expression-try-read)))
 
 (defun lisp-hook ()
+  "Hook applied across modes."
   (paredit-mode)
   (rainbow-delimiters-mode)
   (prettify-symbols-mode)
