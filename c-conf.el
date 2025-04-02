4c00b485d38ba4430d0c9b24a44da0c636b69f4a
beoin <hi.beoin@gmail.com>
2025-04-02 20:07:48 +0300

    require c-ts-mode and set c-ts-mode-emacs-sources-support, c-ts-mode-indent-offset, c-ts-mode-indent-style

--- c-conf.el
+++ c-conf.el
@@ -6,6 +6,12 @@
 ;; Imports
 (require 'cc-mode)
 
+;; tree-sitter support for C and C++
+(require 'c-ts-mode)
+(setq c-ts-mode-emacs-sources-support 't)
+(setq c-ts-mode-indent-offset 2)
+(setq c-ts-mode-indent-style 'gnu)
+
 ;; Vars
 (add-to-list 'auto-mode-alist '("\\.c\\'" . c-ts-mode))
 
