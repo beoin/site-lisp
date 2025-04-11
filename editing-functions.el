4bea6b856a6c0502fa2722bd93a893a503f55561
beoin <hi.beoin@gmail.com>
2025-04-11 11:41:15 +0300

    update copy-def-at-point

--- editing-functions.el
+++ editing-functions.el
@@ -87,13 +87,12 @@ C: The character to zap up to."
     (copy-region-as-kill beg end)
     (goto-char curr)))
 
-(defun copy-def-at-point ()
+ (defun copy-def-at-point ()
   "Copy the current definition under point and add it to the kill ring."
   (interactive)
   (let ((curr (point))
-        (end (goto-char (1- (re-search-forward "[ \t]+\\|\n" nil 'move))))
-        (beg (goto-char (1+ (re-search-backward "[ \t]+\\|\n" nil 'move)))))
-    (copy-region-as-kill beg end)
+        (end (goto-char (1- (re-search-forward "[ \t]+\\|\n" nil 'move)))))
+    (copy-region-as-kill curr end)
     (goto-char curr)))
 
 (defun capitalise-word ()
