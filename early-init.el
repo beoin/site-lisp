ad039f7014334e472aa87a0d8aa52547d67d0003
beoin <hi.beoin@gmail.com>
2025-05-02 10:34:38 +0300

    require modes, group vars

--- early-init.el
+++ early-init.el
@@ -4,26 +4,44 @@
 ;;; Customizations that take effect during Emacs startup.
 ;;; Code:
 
-;; Number of bytes of consing between garbage collections.
+;; alloc.c storage allocation and gc for lisp interpreter
 (setq gc-cons-threshold most-positive-fixnum)
-;; Restore to normal value after startup (e.g. 50MB)
-(add-hook 'emacs-startup-hook
-          (lambda () (setq gc-cons-threshold (* 50 1024 1024))))
+(setq garbage-collection-messages 't)
 
-(setq garbage-collection-messages t)
-(setq load-prefer-newer t)
+;; lread.c lisp parsing and input streams
+(setq load-prefer-newer 't)
+
+;; commands for multilingual environment
+;;(require 'mule-cmds) not provided
 (set-language-environment "UTF-8")
 (setq default-input-method nil)
-(setq inhibit-compacting-font-caches t)
+(set-default-coding-systems 'utf-8)
+
+;; font.c font primitives.
+(setq inhibit-compacting-font-caches 't)
+
+;; eval.c evaluator for lisp interpreter
 (setq lisp-eval-depth-reserve 300)
+
+;; frame.c generic frame functions
 (setq frame-inhibit-implied-resize 't)
 
-;; UI
+;; define a default menu bar
+(require 'menu-bar)
 (menu-bar-mode -1)
+
+;; window system-independent scroll bar support
+(require 'scroll-bar)
 (scroll-bar-mode -1)
+
+;; setting up the tool bar
+(require 'tool-bar)
 (tool-bar-mode -1)
-(setq inhibit-startup-screen t)
-(setq inhibit-spash-screen t)
+
+;; process shell arguments
+;;(require 'startup) not provided
+(setq inhibit-startup-screen 't)
+(setq inhibit-splash-screen 't)
 
 (provide 'early-init)
 ;;; early-init.el ends here
